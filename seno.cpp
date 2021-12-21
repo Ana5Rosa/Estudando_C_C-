db16ac4cde3624da1b252b48d29dd245658b4cd5
Ana5Rosa <anacla.ped.rosa@gmail.com>
2021-12-21 13:59:35 -0300

    Create seno.cpp

---
+++ seno.cpp
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double x_graus, x_radiano, seno_x, expoente;
+    int n_parcelas, sinal_parcela, valor_parcela;
+    long long fatorial;
+
+    do
+    {
+        printf("Digite o valor de X, em graus: ");
+        scanf("%lf", &x_graus);
+        if(x_graus < 0)
+            puts("Erro - X precisa ser maior ou igual a 0");
+    } while(x_graus < 0);
+
+    do
+    {
+        printf("Digite N (quantidade de parcelas): ");
+        scanf("%d", &n_parcelas);
+        if(n_parcelas <= 0)
+            puts("Erro - N precisa ser maior que 0");
+    } while (n_parcelas <= 0);
+
+    // transformar X para radiano
+    x_radiano = (M_PI * x_graus) / 180;
+
+    // looping das parcelas
+    sinal_parcela = 1; // Verifica se a próxima conta soma ou diminui
+
+    valor_parcela = 1;
+
+    for(int i = 1; i <= n_parcelas; i++)
+    {
+        expoente = 1;
+        fatorial = 1;
+
+        //looping expoente
+        for(int i = 1; i <= valor_parcela; i++)
+        {
+            expoente *= x_radiano;
+        }
+
+
+        //looping fatorial
+        for(int i = 1; i <= valor_parcela; i++)
+        {
+            fatorial *= i;
+        }
+
+
+        double conta = expoente/fatorial;
+
+        //Verificando se soma ou diminui
+        if(sinal_parcela == 1)
+        {
+            seno_x = seno_x + conta;
+            sinal_parcela = -1;
+        }
+        else
+        {
+            seno_x = seno_x - conta;
+            sinal_parcela = +1;
+        }
+
+        valor_parcela += 2;
+   }
+
+   printf("sen(%.8lf) = %.8lf\n", x_radiano, seno_x);
+}
